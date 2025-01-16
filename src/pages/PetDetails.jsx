7347bd32b0c4547b19aafdaa929c36a0662b2ae4
sumonDev9 <sumonmit678@gmail.com>
2025-01-16 19:39:19 +0530

    Implemented pet adoption form with phone and address fields, and integrated form submission using Axios

--- src/pages/PetDetails.jsx
+++ src/pages/PetDetails.jsx
@@ -3,78 +3,99 @@ import UseAuth from '../hooks/UseAuth';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
-    Dialog,
-    Input,
-    Textarea,
-  } from "@material-tailwind/react";
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+  Dialog,
+  Input,
+  Textarea,
+} from "@material-tailwind/react";
 import { FaLocationDot } from 'react-icons/fa6';
 import { MdOutlineDateRange } from 'react-icons/md';
 
-// import {
-//   Button,
-//   Dialog,
-//   Card,
-//   CardHeader,
-//   CardBody,
-//   CardFooter,
-//   Typography,
-//   Input,
-//   Checkbox,
-// } from "@material-tailwind/react";
 
 const PetDetails = () => {
-    const [pet, SetPets] = useState({})
-    const {user} = UseAuth();
-    const {id} = useParams();
+  const [pet, SetPets] = useState({})
+  const { user } = UseAuth();
+  const { id } = useParams();
 
-    useEffect(() => {
-        fetchAllPet()
-    }, []);
+  useEffect(() => {
+    fetchAllPet()
+  }, []);
 
-    const fetchAllPet = async () => {
-        const { data } = await axios.get(`http://localhost:5000/pets/${id}`)
-        SetPets(data)
+  const fetchAllPet = async () => {
+    const { data } = await axios.get(`http://localhost:5000/pets/${id}`)
+    SetPets(data)
+  }
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen((cur) => !cur);
+
+
+  // request 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const phone = form.phone.value;
+    const address = form.address.value;
+    console.log(phone)
+    const adoptionData = {
+      petId: pet._id,
+      petName: pet.name,
+      petimage: pet.image,
+      userName: user?.displayName,
+      userEmail: user?.email,
+      phone,
+      address
     }
 
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen((cur) => !cur);
+    console.log(adoptionData )
+
+    await axios.post('http://localhost:5000/adoptions', adoptionData)
+      .then(res => {
+        console.log(res.data)
+        if (res.data.insertedId) {
+          alert('added to your carts')
+
+        }
+      })
+    setOpen(false)
+  }
 
 
-    const {_id, image, name, longDescription, shortDescription, age, location} = pet || {}
-    return (
-     <section className='w-11/12 mx-auto'>
-           <Card className="mt-6 shadow-md max-w-3xl mx-auto">
+
+  const { _id, image, name, longDescription, shortDescription, age, location } = pet || {}
+  return (
+    <section className='w-11/12 mx-auto'>
+      <Card className="mt-6 shadow-md max-w-3xl mx-auto">
         <CardHeader className="relative shadow-none m-0 p-2 md:p-6 md:h-[420px] rounded-none">
           <img
-             className='rounded-md w-full object-cover h-full'
+            className='rounded-md w-full object-cover h-full'
             src={image}
             alt=''
           />
         </CardHeader>
         <CardBody className='pt-0'>
-          <Typography  className="mb-2 text-4xl text-secondary">
+          <Typography className="mb-2 text-4xl text-secondary">
             {name}
           </Typography>
-          <Typography  className="mb-2 text-2xl text-info">
+          <Typography className="mb-2 text-2xl text-info">
             {shortDescription
             }
           </Typography>
           <Typography className='text-info'>
-           {longDescription}
+            {longDescription}
           </Typography>
           <div className='flex gap-5'>
             <Typography className="flex items-center text-info gap-1 dark:text-white mt-1">
-                    <MdOutlineDateRange /> {age}
-                    </Typography>
-                    <Typography className="flex items-center text-info gap-1 dark:text-white mt-1">
-                    <FaLocationDot /> {location}
-                    </Typography>
+              <MdOutlineDateRange /> {age}
+            </Typography>
+            <Typography className="flex items-center text-info gap-1 dark:text-white mt-1">
+              <FaLocationDot /> {location}
+            </Typography>
           </div>
         </CardBody>
         <CardFooter className="pt-0">
@@ -83,64 +104,65 @@ const PetDetails = () => {
       </Card>
 
 
-      {/* modal form */}
+
+      {/* modal */}
       <Dialog
         size="sm"
         open={open}
         handler={handleOpen}
-        className="bg-white shadow  h-[550px] overflow-scroll"
+        className="bg-white shadow h-[550px] overflow-scroll"
       >
-        <Card className="mx-auto w-full shadow-sm rounded-none  max-w-[24rem]">
+
+        <Card className="mx-auto w-full shadow-sm rounded-none max-w-[24rem]">
+        <form onSubmit={handleSubmit}>
           <CardBody className="flex flex-col gap-4">
-            <Typography variant="h4" className='text-center text-secondary'>
+            <Typography variant="h4" className="text-center text-secondary">
               {name}
             </Typography>
-            {/* pet id */}
             <Typography className="-mb-2" variant="h6">
-             Pet ID
+              Pet ID
             </Typography>
-            <Input label="text" defaultValue={_id} disabled={true} size="xl" />
-            {/* Pet name */}
+            <Input label="text" defaultValue={_id} disabled size="xl" />
             <Typography className="-mb-2" variant="h6">
-             Pet name
+              Pet name
             </Typography>
-            <Input label="text" defaultValue={name} disabled={true} size="xl" />
-            {/* Pet image */}
+            <Input label="text" defaultValue={name} disabled size="xl" />
             <Typography className="-mb-2" variant="h6">
-            Pet image
+              Pet image
             </Typography>
-            <Input label="text" defaultValue={image} disabled={true} size="xl" />
-            {/* user name */}
+            <Input label="text" defaultValue={image} disabled size="xl" />
             <Typography className="-mb-2" variant="h6">
-            User name
+              User name
             </Typography>
-            <Input label="text" defaultValue={user?.displayName} disabled={true} size="xl" />
-            {/* user Email */}
+            <Input label="text" defaultValue={user?.displayName} disabled size="xl" />
             <Typography className="-mb-2" variant="h6">
-            User email
+              User email
             </Typography>
-            <Input label="text" defaultValue={user?.email} disabled={true} size="xl" />
-            {/* phone no */}
+            <Input label="text" defaultValue={user?.email} disabled size="xl" />
             <Typography className="-mb-2" variant="h6">
-            Phone no
+              Phone no
             </Typography>
-            <Input label="number" name='phone' size="xl" />
-            {/* Address */}
+            <Input label="number" name="phone" size="xl" />
+            {/* <input type="text" placeholder='Your number' name="phone" /> */}
             <Typography className="-mb-2" variant="h6">
-              Address
-            </Typography>
-            {/* <Input label="text" name='address' className='w-full' size="xl" /> */}
-            <Textarea label="Message" />
+        Address
+      </Typography>
+      <Textarea name="address" label="Address" />
           </CardBody>
           <CardFooter className="pt-0">
-            <Button variant="gradient" onClick={handleOpen} fullWidth>
-              Sign In
-            </Button>
+            
+              <Button variant="gradient"  type="submit" fullWidth>
+                Sign In
+              </Button>
+           
           </CardFooter>
+          </form>
         </Card>
+
       </Dialog>
-     </section>
-    );
+
+    </section>
+  );
 };
 
 export default PetDetails;
