20e188d848a03cd00a6df7060e9e5cb529e03642
sumonDev9 <sumonmit678@gmail.com>
2025-02-03 23:14:28 +0530

    text size updated

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -4,7 +4,7 @@ import { Typography } from '@material-tailwind/react';
 const SectionTitle = ({heading, subHeading}) => {
     return (
         <div className='w-11/12 mx-auto text-center'>
-            <Typography variant="h2" className="text-2xl md:text-3xl dark:text-white  lg:text-4xl font-bold mb-6">
+            <Typography variant="h2" className="text-2xl md:text-3xl text-secondary dark:text-white  lg:text-4xl font-bold mb-6">
                 {heading}
             </Typography>
             <Typography  className="dark:text-white max-w-3xl mx-auto text-base md:text-lg mb-8">
